f2f53f3f9ddf8c3a615d095a4c26eb09f4ea2712
Dancadlim <dancadlim@gmail.com>
2025-11-22 20:55:00 -0300

    Implement authentication functions in auth.py

---
+++ services/auth.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def login_simulado():
+    st.session_state.user = {"email": "usuario_teste@gmail.com", "name": "Chefe"}
+    st.rerun()
+
+def logout():
+    st.session_state.user = None
+    st.session_state.active_project = None
+    st.rerun()
+
+def check_auth():
+    if "user" not in st.session_state:
+        st.session_state.user = None
+    return st.session_state.user
